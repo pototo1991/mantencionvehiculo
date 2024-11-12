c7dc208b62305c4a45d47371cefa191951690aff
walter <whsg27@gmail.com>
2024-11-11 23:59:15 -0300

    Realizar modificaciones en index.html, login.html, main.html y styles.css

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("main/", views.main_view, name="main"),
-    path(
-        "login/", views.custom_login, name="login"
-    ),  # Vista personalizada de inicio de sesión
+    path("login/", views.custom_login, name="login"),
+    path("vehiculos/", views.vehiculos_view, name="vehiculos"),
+    path("combustibles/", views.combustibles_view, name="combustibles"),
+    path("calculos/", views.calculos_view, name="calculos"),
+    path("logout/", include("django.contrib.auth.urls")),
 ]
--- main/views.py
+++ main/views.py
@@ -10,21 +10,31 @@ def main_view(request):
     return render(request, "main.html")
 
 
+def vehiculos_view(request):
+    return render(request, "vehiculos.html")
+
+
+def combustibles_view(request):
+    return render(request, "combustibles.html")
+
+
+def calculos_view(request):
+    return render(request, "calculos.html")
+
+
 def custom_login(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        print(
-            f"Intentando iniciar sesión con usuario: {username}"
-        )  # Log del nombre de usuario
+        print(f"Intentando iniciar sesión con usuario: {username}")
 
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            print("Inicio de sesión exitoso")  # Log de inicio de sesión exitoso
+            print("Inicio de sesión exitoso")
             return redirect("main")
         else:
-            print("Falló el inicio de sesión")  # Log de fallo de inicio de sesión
+            print("Falló el inicio de sesión")
             return render(
                 request,
                 "registration/login.html",
